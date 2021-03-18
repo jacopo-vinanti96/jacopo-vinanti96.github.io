20550b7cf265501bd3767cc76c3879670a334877
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-18 20:52:26 +0100

    To do list -  Replica

--- js/script.js
+++ js/script.js
@@ -1,72 +1,91 @@
-// Genera un numero casuale
-function genRandomNum( max, min ) {
-  return Math.floor( Math.random() * (max - min + 1) ) + min;
-}
+// Dichiarazione variabili
+var addProject = $('#add-project'),
+    addValue,
+    detailsTemplate = $('.template .details-template'),
+    lineTemplate = $('.list-template .list-element'),
+    projectDate,
+    tempTemplate,
+    today = $('#today'),
+    tomorrow = $('#tomorrow'),
+    valid;
 
-// Controllo se il numero è uguale ai precedenti
-function numControl( array, num ) {
-  for ( var j = 0; j < array.length; j++ ) {
-    if ( num == array[j] ) {
-      return true;
-      console.log(num);
-    }
-  }
-  return false;
-}
 
-// Onclick inizia la funzione
-function startGame() {
 
-  // Dichiarazione variabili
-  var bombs = [],
-  maxNum = 100,
-  minNum = 1,
-  nBombe = 16;
-  output = document.getElementById('output'),
-  containerBtn = document.getElementById('buttons-container');
-  userNum = 0,
-  userNumList = [],
-  boom = new Audio('audio/oooh.mp3'),
-  kids = new Audio('audio/hurray.mp3');
+// Dichiarazione funzione di controllo del testo
+function inputControl (word) {
 
-  // Selezione livello e relativi controlli
-  var level = parseInt( prompt("INSERISCI: \n0 = livello facile \n1 = livello normale \n2 = livello difficile") );
-  while ( level < 0 || level > 2 || isNaN( level ) ) {
-    level = parseInt( prompt("Il valore inserito non è valido \nINSERISCI: \n0 = livello difficile \n1 = livello normale \n2 = livello facile") );
-  }
-  if ( level == 1 ) {
-    maxNum = 80;
-  } else if ( level == 2 ) {
-    maxNum = 50;
-  }
-
-  // Ciclo for per generare i numeri con un ciclo for e per controllare che sia diverso dagli altri numeri tramite funzioni
-  var i = 0;
-  while ( i < nBombe ) {
-    newNumber = genRandomNum( maxNum, minNum );
-    if ( numControl( bombs, newNumber ) == false ) {
-      bombs.push(newNumber);
-      i++;
-    }
-  }
-  console.log("Numeri bomba: " + bombs);
-
-  // Ciclo che riceve il numero in input, controlla la validità, controlla se è una bomba o meno. Terminati i numeri sicuri il contatore viene incrementato per fermare il ciclo
-  while ( userNumList.length < maxNum - nBombe ) {
-    userNum = parseInt( prompt("Inserisci un numero da " + minNum + " a " + maxNum ) );
-    if ( userNum < minNum || userNum > maxNum || isNaN( userNum ) ) {
-      alert("Il valore inserito non è valido");
-    } else if ( userNumList.length > 0 && numControl( userNumList, userNum ) == true ) {
-      alert("Devi inserire un numero diverso, non barare!!");
-    } else {
-      userNumList.push(userNum);
-    }
-    if ( numControl( bombs, userNum ) == true ) {
-      boom.play();
-      return output.innerHTML = "Hai perso... Punteggio: " + userNumList.length;
-    } else if ( userNumList.length == maxNum - nBombe ) {
-      kids.play();
-      return output.innerHTML = "Hai vinto!! Complimenti!!";
-    }
+  // Controllo se l' input è vuoto
+  if (word.length === 0 || !word.trim()) {
+    alert("Please enter a word");
+  } else {
+    valid = true;
   }
 }
+
+
+// Onclick addProject legge valore e lo aggiunge come nuova riga di testo
+addProject.on('keydown', function (e) {
+// Assegnazione valid per il controllo
+  valid = false;
+// Se viene premuto enter allora l' algoritmo viene eseguito
+  if ( e.keyCode == 13 ) {
+    addValue = addProject.val();
+    inputControl(addValue);
+// Se l' input passa al controllo si chiede la data
+    if ( valid == true ) {
+      projectDate = prompt("Insert the date\n(Example: today, tomorrow, 31/12/1999)");
+      // Si genera la nuova riga
+      tempTemplate = lineTemplate.clone();
+      tempTemplate.prepend(addValue);
+      // Si controlla se la data corrisponde e si aggiunge la riga in HTML
+      if ( projectDate.toLowerCase() == "today" ) {
+        today.append(tempTemplate);
+
+      } else if ( projectDate.toLowerCase() == "tomorrow" ) {
+        tomorrow.append(tempTemplate);
+      //Altrimenti se la data non è tra le predefinite
+      } else {
+        // Dichiarazione variabili
+        var appended = false;
+        //Le date inserite dall' utente
+        var userDates = $('div:not(.template) > .details-template .summary-title');
+
+        // Se sono state inserite date personalizzate si controlla la data
+        if ( userDates.length > 0 ) {
+          for (var i = 0; i < userDates.length; i++) {
+            // Se la data esiste già allora si aggiunge la riga a questa data
+            if ( $(userDates[i]).text() == projectDate ) {
+
+              $(userDates[i])
+              .parent('.summary-template')
+              .siblings('.list-template')
+              .append(tempTemplate);
+
+              appended = true;
+            }
+          }
+        }
+        // Altrimenti appended non è true quindi la data è nuova
+        if ( appended == false ) {
+          tempTemplate = detailsTemplate.clone();
+          tempTemplate.find('.summary-title').append( projectDate );
+          tempTemplate.find('.list-element').prepend(addValue);
+          $('.list__container').append(tempTemplate);
+        }
+      }
+    }
+  }
+});
+
+$('.list__container').on( 'click', '.list-element', function () {
+  var detailsNotTemplate = $('div:not(.template) > .details-template');
+  $(this).addClass('clicked');
+  setTimeout( function () {
+    $(".list-element.clicked").remove();
+    for (var i = 0; i < detailsNotTemplate.length; i++) {
+      if ( $(detailsNotTemplate[i]).find('.list-element').length == 0 ) {
+        $(detailsNotTemplate[i]).remove();
+      }
+    }
+  }, 1000);
+});
