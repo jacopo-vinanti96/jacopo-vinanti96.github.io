e15100dd622103fbbbdd7d1515cbdeaf34bc1f1e
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-04-18 16:16:47 +0200

    Boolflix - Netflix replica

--- js/script.js
+++ js/script.js
@@ -1,146 +1,138 @@
-// Dichiarazione variabili
-var addProject = $('#add-project'),
-    addValue,
-    date = new Date,
-    detailsTemplate = $('.template .details-template'),
-    lineTemplate = $('.list-template .list-element'),
-    projectDate,
-    tempTemplate,
-    today = $('#today'),
-    tomorrow = $('#tomorrow'),
-    valid;
+// Documentazione API TMDB:
+// https://developers.themoviedb.org/3/search/search-companies
 
-
-
-// Dichiarazione funzione di controllo del testo
-function inputControl (word) {
-
-  // Controllo se l' input è vuoto
-  if (word.length === 0 || !word.trim()) {
-    alert("Please enter a word");
-  } else {
-    valid = true;
-  }
-}
-
-// Date dev' essere il valore di un input date
-function reverseDate (date) {
-  let userDay = date[8] + date[9];
-  let userMonth = date[5] + date[6];
-  let userYear = date[0] + date[1] + date[2] + date[3];
-  return `${userDay}/${userMonth}/${userYear}`;
-}
-
-// Dichiarazione giorno attuale e giorno successivo e conversione
-var month = date.getUTCMonth() + 1; //mesi da 1-12
-
-if ( month < 10 ) {
-  month = `0${month}`;
-  console.log("lunghezza 1");
-}
-
-const day = date.getUTCDate();
-const year = date.getUTCFullYear();
-// Conversione
-const currentDate = `${day}/${month}/${year}`;
-const duedate = `${day + 1}/${month}/${year}`;
-
-
-// Funzione per aggiungere un progetto
-function addProjectLine () {
-  // Assegnazione valid per il controllo
-  valid = false;
-  // Assegnazione valore dell' input
-  addValue = addProject.val();
-  //Controllo dell' input
-  inputControl(addValue);
-  // Se l' input passa al controllo si chiede la data
-  if ( valid == true ) {
-    // Input data dell' utente
-    projectDate = $('.user-date').val();
-    // Si modifica la data
-    projectDate = reverseDate(projectDate);
-
-    console.log(currentDate);
-    console.log(duedate);
-
-    // Si genera la nuova riga
-    tempTemplate = lineTemplate.clone();
-    tempTemplate.prepend(addValue);
-    // Si controlla se la data corrisponde e si aggiunge la riga in HTML
-    if ( projectDate == currentDate ) {
-      today.append(tempTemplate);
-
-    } else if ( projectDate == duedate ) {
-      tomorrow.append(tempTemplate);
-    //Altrimenti se la data non è tra le predefinite
-    } else {
-      // Dichiarazione variabili
-      var appended = false;
-      //Le date inserite dall' utente
-      var userDates = $('div:not(.template) > .details-template .summary-title');
-
-      // Se sono state inserite date personalizzate si controlla la data
-      if ( userDates.length > 0 ) {
-        for (var i = 0; i < userDates.length; i++) {
-          // Se la data esiste già allora si aggiunge la riga a questa data
-          if ( $(userDates[i]).text() == projectDate ) {
-
-            $(userDates[i])
-            .parent('.summary-template')
-            .siblings('.list-template')
-            .append(tempTemplate);
-
-            appended = true;
-          }
+const app = new Vue ({
+  el: '#app',
+  data: {
+    baseURL: 'https://api.themoviedb.org/3/search/',
+    paramsAxios: {
+      params: {
+        language: 'it-IT',
+        api_key: 'afbb7def2542bc20b7d5bfd24baf9b6a',
+        query: '',
+        page: 1,
+      }
+    },
+    moviesQuery: [],
+    tvSeriesQuery: [],
+    moviesQueryShow: [],
+    tvSeriesQueryShow: [],
+    TitleInput: '',
+    moviePages: 1,
+    tvPages: 1,
+    flags: ['en', 'de', 'es', 'fr', 'it', 'pt'],
+    btnMovSelected: 1,
+    btnTvSelected: 1,
+  },
+  methods: {
+    apiGet(type, page) {
+      this.paramsAxios.params.page = page;
+      this.paramsAxios.params.query = this.TitleInput;
+      return axios.get( this.baseURL + type, this.paramsAxios );
+    },
+    search() {
+      if ( document.getElementById('title-search').value != '' ) {
+        this.TitleInput = document.getElementById('title-search').value;
+        if ( this.TitleInput != '' ) {
+          this.getMovies();
+          this.getSeries();
         }
       }
-      // Altrimenti appended non è true quindi la data è nuova
-      if ( appended == false ) {
-        tempTemplate = detailsTemplate.clone();
-        tempTemplate.find('.summary-title').append( projectDate );
-        tempTemplate.find('.list-element').prepend(addValue);
-        $('.list__container').append(tempTemplate);
+    },
+    getMovies(page = 1) {
+      this.apiGet('movie', page)
+      .then( (arr) => {
+        this.moviesQuery = arr.data.results;
+        this.moviePages = arr.data.total_pages;
+        this.btnMovSelected = page;
+        this.convertVoteAvg(this.moviesQuery);
+        this.moviesQueryShow = [];
+        this.setMoviesIndex();
+      });
+    },
+    getSeries(page = 1) {
+      this.apiGet('tv', page)
+      .then( (arr) => {
+        this.tvSeriesQuery = arr.data.results;
+        this.tvPages = arr.data.total_pages;
+        this.btnTvSelected = page;
+        this.convertVoteAvg(this.tvSeriesQuery);
+        this.tvSeriesQueryShow = [];
+        this.setTvIndex();
+      });
+    },
+    setMoviesIndex() {
+      let i = 0;
+      while ( i < 4 && i < this.moviesQuery.length ) {
+        this.moviesQueryShow.push(i);
+        i++;
       }
-    }
-    addProject.val("");
-  }
-}
-
-
-// Onclick addProject legge valore e lo aggiunge come nuova riga di testo
-addProject.on('keydown', function (e) {
-// Se viene premuto enter allora l' algoritmo viene eseguito
-  if ( e.keyCode == 13 ) {
-    addProjectLine();
-    $('.instruction').removeClass('visible');
-    firstKeyDown = true;
-  }
-});
-
-// Funzione per rendere visibile le istruzioni qualora l' input non sia vuoto
-var firstKeyDown = true;
-
-addProject.on('keyup', function (e) {
-
-  if ( addProject.val() == "" ) {
-    $('.instruction').removeClass('visible');
-    firstKeyDown = true;
-  } else if ( firstKeyDown = true ) {
-    $('.instruction').addClass('visible');
-    firstKeyDown = false;
-  }
-});
-
-$('.list__container').on( 'click', '.list-element', function () {
-  var detailsNotTemplate = $('div:not(.template) > .details-template');
-  $(this).addClass('clicked');
-  setTimeout( function () {
-    $(".list-element.clicked").remove();
-    for (var i = 0; i < detailsNotTemplate.length; i++) {
-      if ( $(detailsNotTemplate[i]).find('.list-element').length == 0 ) {
-        $(detailsNotTemplate[i]).remove();
+    },
+    nextMoviesIndex() {
+      if ( this.moviesQueryShow[ this.moviesQueryShow.length - 1 ] != this.moviesQuery.length - 1 ) {
+        let i = 0;
+        const iMov = this.moviesQueryShow[ this.moviesQueryShow.length - 1 ];
+        this.moviesQueryShow = [];
+        while ( i < 4 && iMov + i < this.moviesQuery.length - 1 ) {
+          this.moviesQueryShow[i] = iMov + 1 + i;
+          i++;
+        }
       }
-    }
-  }, 1000);
+      this.$forceUpdate();
+    },
+    prevMoviesIndex() {
+      if ( this.moviesQueryShow[0] != 0 ) {
+        let i = 0;
+        let iMov = this.moviesQueryShow[0];
+        while ( i < 4 && iMov - i > 0 ) {
+          this.moviesQueryShow[i] = iMov - 1 - i;
+          i++;
+        }
+      }
+      this.moviesQueryShow.sort(function(n1, n2) {
+        return n1 - n2;
+      });
+      this.$forceUpdate();
+    },
+    setTvIndex() {
+      let i = 0;
+      while ( i < 4 && i < this.tvSeriesQuery.length ) {
+        this.tvSeriesQueryShow.push(i);
+        i++;
+      }
+    },
+    nextTvIndex() {
+      if ( this.tvSeriesQueryShow[ this.tvSeriesQueryShow.length - 1 ] != this.tvSeriesQuery.length - 1 ) {
+        console.log('ciao');
+        let i = 0;
+        const iMov = this.tvSeriesQueryShow[ this.tvSeriesQueryShow.length - 1 ];
+        this.tvSeriesQueryShow = [];
+        while ( i < 4 && iMov + i < this.tvSeriesQuery.length - 1 ) {
+          this.tvSeriesQueryShow[i] = iMov + 1 + i;
+          i++;
+        }
+      }
+      this.$forceUpdate();
+    },
+    prevTvIndex() {
+      if ( this.tvSeriesQueryShow[0] != 0 ) {
+        let i = 0;
+        let iMov = this.tvSeriesQueryShow[0];
+        while ( i < 4 && iMov - i > 0 ) {
+          this.tvSeriesQueryShow[i] = iMov - 1 - i;
+          i++;
+        }
+      }
+      this.tvSeriesQueryShow.sort(function(n1, n2) {
+        return n1 - n2;
+      });
+      this.$forceUpdate();
+    },
+    convertVoteAvg(thisArr) {
+      thisArr.forEach( (movie) => {
+        const rating = Math.round( movie.vote_average / 2 );
+        movie['rating'] = rating;
+      })
+    },
+  }
 });
